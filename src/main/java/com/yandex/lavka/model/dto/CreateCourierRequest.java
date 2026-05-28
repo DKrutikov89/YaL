c8f8ab7549b6f6d0c411d6b18a34280f02183163
DKrutikov89 <itdkruti@gmail.com>
2026-05-28 16:53:37 +0300

    create DTO Courier Request

---
+++ src/main/java/com/yandex/lavka/model/dto/CreateCourierRequest.java
@@ -0,0 +1,18 @@
+package com.yandex.lavka.model.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateCourierRequest {
+
+    @NotEmpty(message = "Couriers list cannot be empty")
+    private List<@Valid CreateCourierDto> couriers;
+}
