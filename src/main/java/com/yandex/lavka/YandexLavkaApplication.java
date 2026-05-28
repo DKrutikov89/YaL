f2718ca81de4f7200509a704b27c84e8d66ac3b1
DKrutikov89 <itdkruti@gmail.com>
2026-05-28 14:12:35 +0300

    create a new project and save the first commit

---
+++ src/main/java/com/yandex/lavka/YandexLavkaApplication.java
@@ -0,0 +1,13 @@
+package com.yandex.lavka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexLavkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YandexLavkaApplication.class, args);
+	}
+
+}
